67dba774bfd13d0f757199401a050f96d0fd0312
Hiroki Kato <cockscomb@hatena.ne.jp>
2023-06-20 10:08:45 +0900

    NavigationStack example

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -5,12 +5,20 @@ private let appleDeveloper = URL(string: "https://developer.apple.com")!
 
 struct ContentView: View {
     var body: some View {
-        List {
-            Link("Open in Safari", destination: appleDeveloper)
-            Link("Open in SFSafariViewController", destination: appleDeveloper)
-                .openURLInSafariView()
-            Text("Open in SFSafariViewController with [Attributed String](https://developer.apple.com)")
-                .openURLInSafariView()
+        NavigationStack {
+            List {
+                Link("Open in Safari", destination: appleDeveloper)
+                Link("Open in SFSafariViewController", destination: appleDeveloper)
+                    .openURLInSafariView()
+                Text("Open in SFSafariViewController with [Attributed String](https://developer.apple.com)")
+                    .openURLInSafariView()
+                NavigationLink("Open in SFSafariViewController within NavigationStack", value: appleDeveloper)
+            }
+            .navigationDestination(for: URL.self) { url in
+                SafariView(url: url)
+                    .toolbar(.hidden)
+                    .edgesIgnoringSafeArea(.all)
+            }
         }
     }
 }
